f18e2f4a0d273689a14998871bf39c5bf53e8162
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-07-07 15:46:14 +0300

    feat: render list of 10 shades with values.js. add unique ids to shades with nano id

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import Form from './Form.jsx';
 import ColorList from './ColorList.jsx';
+import Values from 'values.js';
 
 const App = () => {
+	const [colors, setColors] = useState(new Values('#2b5b73').all(10));
+
 	return (
 		<main>
 			<Form />
-			<ColorList />
+			<ColorList colors={colors} />
 		</main>
 	);
 };
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const SingleColor = () => {
-	return <div>Single color</div>;
+const SingleColor = ({ index, color }) => {
+	const { hex, weight } = color;
+
+	return (
+		<article
+			className={index > 10 ? 'color color-light' : 'color'}
+			style={{ backgroundColor: `#${hex}` }}
+		>
+			<p className="percent-value">{weight}%</p>
+			<p className="color-value">#{hex}</p>
+		</article>
+	);
 };
 
 export default SingleColor;
