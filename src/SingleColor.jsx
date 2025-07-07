ea057e3c6d31be6cbba095a6ea9108e5495410d9
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-07-07 21:56:49 +0300

    feat: add copy color functionality. add notifications with react-toastify library

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const SingleColor = ({ index, color }) => {
 	const { hex, weight } = color;
+	const saveToClipboard = async () => {
+		if (navigator.clipboard) {
+			await navigator.clipboard.writeText(`#${hex}`);
+			try {
+				toast.success(`Color #${hex} copied to clipboard!`);
+			} catch (error) {
+				toast.error('Failed to copy color to clipboard!');
+			}
+		} else {
+			toast.error('Clipboard access is not available!');
+		}
+	};
 
 	return (
 		<article
+			onClick={saveToClipboard}
 			className={index > 10 ? 'color color-light' : 'color'}
 			style={{ backgroundColor: `#${hex}` }}
 		>
